1e1913bd534ba6e2a652b25c3629442969266274
Hansuero <93417959+Hansuero@users.noreply.github.com>
2023-05-18 12:55:16 +0800

    添加用户模型

--- COMusic/user/models.py
+++ COMusic/user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    user_name = models.CharField('用户名', max_length=30)
+    password = models.CharField('密码', max_length=32)
+    email = models.EmailField()
+    photo = models.FileField('用户头像', upload_to='')
+    photo_url = models.CharField('用户头像路径', max_length=128, default='')
+    bio = models.CharField('个人简介', max_length=256, default='')
+
+    class Meta:
+        db_table = 'user'
