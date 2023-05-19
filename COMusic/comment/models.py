e74b2e0237eef08cea8bd65ab313c1ba4331f516
Hansuero <93417959+Hansuero@users.noreply.github.com>
2023-05-19 12:10:42 +0800

    添加评论模型

--- COMusic/comment/models.py
+++ COMusic/comment/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from user.models import User
+from music.models import Song
+
 
 # Create your models here.
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content = models.CharField('评论内容', max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'comment'
+
