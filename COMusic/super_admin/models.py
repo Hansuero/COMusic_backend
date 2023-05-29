c04bb36afbe97f6f427e01f891db200087198519
Hansuero <93417959+Hansuero@users.noreply.github.com>
2023-05-29 11:43:59 +0800

    report修改为message

--- COMusic/super_admin/models.py
+++ COMusic/super_admin/models.py
@@ -8,12 +8,11 @@ from user.models import User
 # Create your models here.
 
 
-class Report(models.Model):  # 举报信息
-    reporter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reports_sent')  # 发送方
-    admin = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reports_received')  # 管理员，作为举报信息的接收者
-    song = models.ForeignKey(Song, on_delete=models.CASCADE, blank=True, null=True)
-    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE, blank=True, null=True)
+class Message(models.Model):  # 举报信息
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reports_sent')  # 发送方
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reports_received')  # 管理员，作为举报信息的接收者
+    content = models.TextField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        db_table = 'report'
+        db_table = 'message'
